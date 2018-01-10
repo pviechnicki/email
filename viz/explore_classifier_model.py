91b7f3d53b701a092a2835a72792d726c95464a2
Peter Viechnicki <pviechnicki@deloitte.com>
2018-01-10 20:30:54 +0000

    improvements to classifier viz

--- viz/explore_classifier_model.py
+++ viz/explore_classifier_model.py
@@ -4,6 +4,9 @@ for emails. Will display what emails were successfully categorized and which
 weren't and which terms are most important for classifier.
 '''
 
+#----------------------------------------------------------------------#
+# Libraries and dependencies                                           #
+#----------------------------------------------------------------------#
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -17,75 +20,64 @@ import sys
 import os
 from flask import send_from_directory
 import pickle
+from email_utils import highlightTerms, formatEmail
+from email_utils import generateAccuracyTable,generateTruthTable
+from email_utils import countTokens, countFreqs
+import nltk
+from nltk.corpus import stopwords
+
 
 #------------------------------------------------------#
 # Prepare input data                                   #
+#Load results from classifier notebook
 #------------------------------------------------------#
-thisDocTFIDF = defaultdict(float)
-
-thisDocTFIDF['red'] = .7
-thisDocTFIDF['green'] = .5
-thisDocTFIDF['blue'] = .2
-thisDocTFIDF['orange'] = 0
-
-with open('../dotce/informativeTerms.pyc', 'rb') as f:
-    allDocsTFIDF = pickle.load(f)
+with open('../classify/totalTermCounts.pyc', 'rb') as f:
+    totalTermCounts = pickle.load(f)
 f.close()
-with open('../dotce/classifierStats.pyc', 'rb') as f:
+with open('../classify/informativeTerms.pyc', 'rb') as f:
+    informativeTerms = pickle.load(f)
+f.close()
+with open('../classify/classifierStats.pyc', 'rb') as f:
     classifierStats = pickle.load(f)
 f.close()
-with open('../dotce/classifierTestResults.pyc', 'rb') as f:
-    classiferTestResults = pickle.load(f)
+with open('../classify/classifierTestResults.pyc', 'rb') as f:
+    classifierTestResults = pickle.load(f)
+f.close()
+with open('../classify/feature_counts_training.pyc', 'rb') as f:
+    feature_counts_training = pickle.load(f)
+f.close()
+with open('../classify/feature_counts_test.pyc', 'rb') as f:
+    feature_counts_test = pickle.load(f)
+f.close()
     
-myString = ('red ' * 40) + 'green ' + 'blue'
+#-------------------------------------------------------------------------#
+# Make 4 chunks of the test data, depending on truth value                #
+#-------------------------------------------------------------------------#
+resultsDF_tp = classifierTestResults[classifierTestResults['truthValue'] == 'truePositive']
+resultsDF_fp = classifierTestResults[classifierTestResults['truthValue'] == 'falsePositive']
+resultsDF_tn = classifierTestResults[classifierTestResults['truthValue'] == 'trueNegative']
+resultsDF_fn = classifierTestResults[classifierTestResults['truthValue'] == 'falseNegative']
+
 
 responseTypes = ['truePositive', 'trueNegative', 'falsePositive', 'falseNegative']
 
-#------------------------------------------------#
-# Return rows and cells of classifier accuracy   #
-#------------------------------------------------#
-def generateAccuracyTable():
-    '''Returns 2-row table with labels in column 1, accuracy, error rate'''
-    return html.Table([
-        html.Tr([
-            html.Th("Accuracy"),
-            html.Td(classifierStats['accuracy'])
-        ]),
-        html.Tr([
-            html.Th("Error Rate"),
-            html.Td(classifierStats['errorRate'])
-        ])
-    ])
-#--------------------------------------------------#
-# Return rows and cells for classifier truth table #
-#--------------------------------------------------#
-def generateTruthTable():
-    return html.Table([
-        html.Tr([
-            html.Th(),
-            html.Th(),
-            html.Th("True Value", style={'colspan': '2'})
-        ]),
-        html.Tr([
-            html.Th(),
-            html.Th(),
-            html.Th("Personal"),
-            html.Th("Not Personal")
-        ]),
-        html.Tr([
-            html.Th(children="Classifier Assigned", style={'rowSpan': 2}),
-            html.Th("Personal"),
-            html.Td(classifierStats['truePositive']),
-            html.Td(classifierStats['falsePositive'])
-        ]),
-        html.Tr([
-            html.Th(),
-            html.Th("Not Personal"),
-            html.Td(classifierStats['falseNegative']),
-            html.Td(classifierStats['trueNegative'])
-        ])
-    ])
-#Local version of stylesheet from https://codepen.io/chriddyp/pen/bWLwgP.css
+#-------------------------------------------------------------------#
+# Store top 10 terms in list for easy access                        #
+#-------------------------------------------------------------------#
+visibleTermsList = list(informativeTerms['feature'].iloc[0:10])
+numEmailsInSelectedDF = resultsDF_tp.shape[0]
+emailPointer = 1
+
+#Highlight the terms in the email which are in the visible list
+highlightedEmailSubject = highlightTerms(resultsDF_tp.iloc[(emailPointer - 1)].subject, visibleTermsList)
+highlightedEmailBody = highlightTerms(resultsDF_tp.iloc[(emailPointer - 1)].body, visibleTermsList)
+subjectPlusBody = (resultsDF_tp.iloc[(emailPointer -1)].subject + " " + resultsDF_tp.iloc[(emailPointer - 1)].body)
+currentTextTokenLength = countTokens(subjectPlusBody)
+currentTextTermFreqs = countFreqs(visibleTermsList, subjectPlusBody)
+
+#----------------------------------------------------------------------------#
+#Local version of stylesheet from https://codepen.io/chriddyp/pen/bWLwgP.css #
+#----------------------------------------------------------------------------#
 stylesheets = ['bWLwgP.css']
 
 #--------------------------------------------#    
@@ -121,13 +113,13 @@ app.layout = html.Div(children = [
                                   id="accuracy_table_div",
                                   children = [
                                       html.H2("Overall Classifier Performance"),
-                                      generateAccuracyTable()
+                                      generateAccuracyTable(classifierStats)
                                       ],
                                   className = "six columns"
                                   ),
                               html.Div(
                                   id = "truth_table_div",
-                                  children = generateTruthTable(),
+                                  children = generateTruthTable(classifierStats),
                                   className = "six columns"
                                   )
                           ],
@@ -140,22 +132,27 @@ app.layout = html.Div(children = [
             html.Label("Show me..."),
             dcc.RadioItems(
                 id="showMe",
-                options = [{'label': responseType, 'value': responseType } for responseType in responseTypes ],
+                options = [{'label': "{}s".format(responseType), 'value': responseType } for responseType in responseTypes ],
                 value = 'truePositive',
                 labelStyle={'display': 'inline-block'}
             )
         ]),
         html.Table(
             html.Tr(children=[
-                html.Td(children = html.Button(id='buttonPrevious', children='Previous')),
-                html.Td(id="emailNumberText", children = "Showing email #1 of N"),
-                html.Td(children = html.Button(id='buttonNext', children='Next'))
+                html.Td(children = html.Button(id='buttonPrevious', children='<<Previous<<')),
+                html.Td(id="emailNumberText", children = "Showing email #{} of {}".format(emailPointer, numEmailsInSelectedDF)),
+                html.Td(children = html.Button(id='buttonNext', children='>>Next>>'))
             ])
         ),
         html.Div(id="text_div", children=[
-            html.H3(id="emailSubject", children="Subject Line"),
-            html.P(myString)
-        ], style={'overflow-y': 'scroll', 'height': '200px'})
+            html.Iframe(
+                id = 'email_text_iframe',
+                sandbox='',
+                srcDoc = formatEmail(highlightedEmailSubject,
+                                     highlightedEmailBody),
+                style = {'width': '450px', 'height': '250px'}
+                )
+        ], style= {'height':'200px'})
         ]),
         html.Div(id="graph_div", className = "six columns", children=[
             dcc.Graph(
@@ -163,41 +160,91 @@ app.layout = html.Div(children = [
                 figure={
                     'data': [
                         go.Bar(
-                            x = [1, 2, 3],
-                            y = ['term1', 'term2', 'term3'],
-                            name = 'Within this email',
+                            x = [(feature_counts_training[term]/totalTermCounts['trainTokensTotal'] * 1000)
+                                 for term in visibleTermsList],
+                            y = visibleTermsList,
+                            name = 'Training Corpus',
                             orientation = 'h',
                             width = .2
                         ),
                         go.Bar(
-                            x = [1.5, .4, 2],
-                            y = ['term1', 'term2', 'term3'],
-                            name = 'All emails',
+                            x = [(feature_counts_test[term]/totalTermCounts['testTokensTotal'] * 1000)
+                                 for term in visibleTermsList],
+                            y = visibleTermsList,
+                            name = 'Test Corpus',
                             orientation = 'h',
                             width = .2
                         ),
+                        go.Bar(
+                            x = [(currentTextTermFreqs[term]/currentTextTokenLength * 1000) for term in visibleTermsList],
+                            y = visibleTermsList,
+                            name = 'This Email',
+                            orientation = 'h',
+                            width = .2
+                        )
                     ],
                     'layout': go.Layout(
-                        xaxis= {'title': 'Term Weight (TF/IDF)', 'autorange': True},
+                        xaxis= {'title': 'Occurrences per 1000 word tokens', 'autorange': True},
                         yaxis= {'type': 'category', 'autorange': True},
-                        width=400,
+                        width=500,
+                        height=500,
                         title='Most Informative Terms',
-                        barmode='group',
-                        bargap = 0.01,
-                        bargroupgap=0,
-                        autosize=True
+                        barmode='group'
                     )
                 }
             )
-        ])
+        ], style={'height': '800px'}) #Text_div
     ], className = "row")
 ])
 
 
 #-------------------------------------------------------------------#
-# Define interactive behaviors from dropdown                        #
+# Define interactive behaviors from inputs                          #
 #-------------------------------------------------------------------#
 
+#-------------------------------------------------------------------#
+# 2 callbacks for radio button to select email subset
+#-------------------------------------------------------------------#
+@app.callback(Output('emailNumberText', 'children'),
+              [Input('showMe', 'value')])
+def update_df_selection(input1):
+    global resultsDF_tp
+    global resultsDF_tn
+    global resultsDF_fn
+    global resultsDF_fp
+    
+    if input1 == 'truePositive':
+        selectedDF = resultsDF_tp
+    elif input1 == 'falsePositive':
+        selectedDF = resultsDF_fp
+    elif input1 == 'falseNegative':
+        selectedDF = resultsDF_fn
+    elif input1 == 'trueNegative':
+        selectedDF = resultsDF_tn
+        
+    return ("Showing email #{} of {}".format(1, selectedDF.shape[0]))
+@app.callback(Output('email_text_iframe', 'srcDoc'),
+              [Input('showMe', 'value')])
+def update_displayed_email_text(input1):
+    global resultsDF_tp
+    global resultsDF_tn
+    global resultsDF_fn
+    global resultsDF_fp
+    global emailPointer
+    
+    if input1 == 'truePositive':
+        selectedDF = resultsDF_tp
+    elif input1 == 'falsePositive':
+        selectedDF = resultsDF_fp
+    elif input1 == 'falseNegative':
+        selectedDF = resultsDF_fn
+    elif input1 == 'trueNegative':
+        selectedDF = resultsDF_tn
+        
+    highlightedEmailSubject = highlightTerms(selectedDF.iloc[(emailPointer - 1)].subject, visibleTermsList)
+    highlightedEmailBody = highlightTerms(selectedDF.iloc[(emailPointer - 1)].body, visibleTermsList)
+    return(formatEmail(highlightedEmailSubject, highlightedEmailBody))
+           
 if __name__ == '__main__':
     app.run_server(debug=True)
 
