f0faebf6a36d4d4b4f2c31052fea7c527ef4f873
Emily Bicks <embicks@deloitte.com>
2018-02-08 15:45:29 -0500

    Emily pushing model structure

--- model/model.py
+++ model/model.py
@@ -1,23 +1,19 @@
 import pandas as pd 
 import sys
 sys.path.insert(0,'C:/Users/ComputerA/email_marker/REPO/data/Output')
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 from model_utils import remove_empty_emails
-from model_utils import create_train_test
-from model_utils import preprocess
-from model_utils import myTokenize
-from model_utils import Instantiate_TFIDF_Vectorizer
-from model_utils import Train_Vectorizer
-from model_utils import Test_Vectorizer
-from model_utils import Logistic_Regression
-from model_utils import Naive_Bayes
-from model_utils import accuracy
-from model_utils import error_rate
-from model_utils import analyze_results
-from model_utils import show_most_informative_features
+from model_utils import train_test_set
+from model_utils import create_doc_matrices
+from model_utils import create_naive_bayes
+from model_utils import NB_results
+from model_utils import create_informative_terms
 
 
 
-email_df = pd.read_excel('C:/Users/ComputerA/email_marker/REPO/data/Output/email_df.xlsx', sheetname = 'Sheet1')
+
+email_df = pd.read_csv('C:/Users/ComputerA/email_marker/REPO/data/Output/Master_df.csv')
 
 #creates sample df
 ## removes empty emails
@@ -25,25 +21,15 @@ email_df = pd.read_excel('C:/Users/ComputerA/email_marker/REPO/data/Output/email
 non_empty_df = remove_empty_emails(email_df)
 
 #divides email df into a test and train set
-train_df, test_df, class_labels_training, class_labels_test = create_train_test(non_empty_df)
-
-
-#creates a Tfidf vectorizer using the stemmed words
-vectorizer= Instantiate_TFIDF_Vectorizer()
-
-#
-train_X, feature_names = Train_Vectorizer(train_df, vectorizer)
-
-test_X = Test_Vectorizer(test_df, vectorizer)
-
-p = Logistic_Regression(train_X, test_X, class_labels_training, class_labels_test)
-
-results, p2, model_nb = Naive_Bayes(train_X, test_X, class_labels_training, class_labels_test)
-
-true_positives, true_negatives, false_negatives, false_positives = analyze_results(test_df, p, p2, class_labels_test)
-
-_ = show_most_informative_features(vectorizer, model_nb, n=20)
+train_df, test_df, class_labels_test, class_labels_training, value_counts = train_test_set(non_empty_df)
 
 
 
 
+train_X, test_X, feature_names = create_doc_matrices(train_df, test_df)
+
+model_nb = create_naive_bayes(train_X, class_labels_training)
+
+predictions, results = NB_results(model_nb, test_X, class_labels_test, test_df)
+
+informativeTerms, feature_counts_training, totalTermCounts = create_informative_terms(train_X, train_df, feature_names, test_df)
--- model/model_utils.py
+++ model/model_utils.py
@@ -1,55 +1,157 @@
-import pandas as pd 
+import pandas as pd
 import os
 import numpy as np
 import sys
 sys.path.append('C:\\Users\\ComputerA\\AppData\\Local\\Continuum\\Anaconda3\\envs\\dotce\\lib\\site-packages\\pyLDAvis')
 from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import Counter
+from collections import defaultdict
 import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-from sklearn import metrics
 from sklearn.cross_validation import train_test_split
-from sklearn.linear_model import LogisticRegression as LR
 from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+import pickle
 
 def remove_empty_emails(email_df):
-	'''
-	creates sample df with 80% of non empty emails and creates personal column to indicate whether the email was originally tagged as personal
-	'''
+	#Add rowid
+	email_df['rownum'] = range(0, len(email_df))
 	email_df.groupby('sensitivity').count()
 	# Filter out empty rows
-	non_empty_df = email_df[email_df['body'].isnull() == False].sample(frac=.8)
+	non_empty_df = email_df[email_df['body'].isnull() == False].sample(frac = 0.9)
 	#sample method chooses a random sample of the origina frame
 	#https://stackoverflow.com/questions/29576430/shuffle-dataframe-rows
 	print("Original dataframe contains {} messages\nNon-empty datafram contains {} messages\n".format(
 	len(email_df), len(non_empty_df)))
+
 	non_empty_df['personal'] = (non_empty_df['sensitivity'] == 'Sensitivity Personal')
 	#Create a vector of class labels
 	class_labels = non_empty_df['personal']
 	#use value_counts() method of series
-	class_labels.value_counts()
+	print(class_labels.value_counts())
+	print(set(non_empty_df['personal']))
 
 	return non_empty_df
 
-def create_train_test(non_empty_df):
-	'''
-	defines train and test set, gets list of all values in personal column for both training and test set to be used in models
-	'''
-
+def train_test_set(non_empty_df):
+	#Create a training set and test set, 80% 20%
 	train_df, test_df = train_test_split(non_empty_df, train_size = 0.8, random_state=44)
 	class_labels_training = list(train_df['personal'])
 	class_labels_test = list(test_df['personal'])
 	value_counts = nltk.FreqDist(class_labels_training)
 
-	return train_df, test_df, class_labels_training, class_labels_test
+	return train_df, test_df, class_labels_test, class_labels_training, value_counts
 
+
+def create_doc_matrices(train_df, test_df):
+	##Instantiate a TFidf vectorizer
+	vectorizer = TfidfVectorizer(sublinear_tf=True, encoding='utf-8', 
+                             max_df=0.5, tokenizer=myTokenize)
+	#This step can take a while
+	train_X = vectorizer.fit_transform(train_df['body'])
+	##Save feature names in a separate list
+	feature_names = vectorizer.get_feature_names()
+	#From http://fastml.com/classifying-text-with-bag-of-words-a-tutorial/
+	#Create another matrix of tfidf scores for the documents in the test set
+	test_X = vectorizer.transform(test_df['body'])
+
+	return train_X, test_X, feature_names
+
+def create_naive_bayes(train_X, class_labels_training):
+	#The alpha value is the sensitivity parameter.
+	# We train the classifier by feeding it with the labeled training data we created in step 3 above.
+	model_nb = MultinomialNB(alpha=0.05)
+	model_nb.fit(train_X, class_labels_training)
+
+	return model_nb
+
+def NB_results(model_nb, test_X, class_labels_test, test_df):
+	predictions = model_nb.predict( test_X )
+	print("Accuracy score for your classifier: {:.3f}\n".format(model_nb.score( test_X, class_labels_test)))
+	print("Error rate for your classifier: {:.3f}\n".format(1-model_nb.score( test_X, class_labels_test)))
+	classifierStats = dict()
+	classifierStats['accuracy'] = model_nb.score( test_X, class_labels_test)
+	classifierStats['errorRate'] = (1 - model_nb.score( test_X, class_labels_test))
+
+	results = [(class_labels_test[i], predictions[i]) for i in range(0,len(predictions))]
+	#Add in the email id, subject, and body, then truePos, falsePos, trueNeg, falseNeg
+	enrichedResults = pd.DataFrame.from_records(results, test_df['rownum'].tolist(), 
+    columns = ['ground_truth', 'predicted_value'])
+	enrichedResults['truthValue'] = enrichedResults.apply(lambda row: truth_value(row), axis=1)
+	enrichedResults['subject'] = test_df['subject'].tolist()
+	enrichedResults['body'] = test_df['body'].tolist()
+	counts = enrichedResults['truthValue'].value_counts()
+	for i in range(0,len(counts)):
+		classifierStats[counts.index[i]] = counts[i]
+	with open('C:/Users/ComputerA/email_marker/REPO/data/Output/classifierStats.pyc', 'wb') as f:
+		pickle.dump(classifierStats, f)
+	f.close()
+	with open('C:/Users/ComputerA/email_marker/REPO/data/Output/classifierTestResults.pyc', 'wb') as f1:
+		pickle.dump(enrichedResults, f1)
+
+	f1.close()
+
+	print(metrics.classification_report(class_labels_test, predictions))
+
+	return predictions, results
+
+def create_informative_terms(train_X, train_df, feature_names, test_df):
+	informativeTerms = top_feats_by_class(train_X, train_df, feature_names, top_n=100)
+	#Should print out 10 most informative features for you
+	informativeTerms.head(10)
+	with open('C:/Users/ComputerA/email_marker/REPO/data/Output/informativeTerms.pyc', 'wb') as f:
+		pickle.dump(informativeTerms, f)
+	f.close()
+
+	feature_counts_training = defaultdict(int)
+	feature_counts_test = defaultdict(int)
+	informativeTermsSet = set(informativeTerms['feature'])
+	trainTokensTotal = 0
+	testTokensTotal = 0
+
+	for index, row in train_df.iterrows():
+	    for token in myTokenize(row['body']):
+	        trainTokensTotal += 1
+	        if (token in informativeTermsSet):
+	            feature_counts_training[token] += 1 
+
+	for index, row in test_df.iterrows():
+	    for token in myTokenize(row['body']):
+	        testTokensTotal+= 1
+	        if (token in informativeTermsSet):
+	            feature_counts_test[token] += 1 
+
+	#Write to disk
+	with open('C:/Users/ComputerA/email_marker/REPO/data/Output/feature_counts_training.pyc', 'wb') as f:
+	    pickle.dump(feature_counts_training, f)
+	f.close()
+	with open('C:/Users/ComputerA/email_marker/REPO/data/Output/feature_counts_test.pyc', 'wb') as f:
+	    pickle.dump(feature_counts_test, f)
+	f.close()
+
+	print(informativeTermsSet)
+
+	totalTermCounts = {'trainTokensTotal': trainTokensTotal, 'testTokensTotal': testTokensTotal}
+	with open('C:/Users/ComputerA/email_marker/REPO/data/Output/totalTermCounts.pyc', 'wb') as f:
+		pickle.dump(totalTermCounts, f)
+	f.close()
+
+	return informativeTerms, feature_counts_training, totalTermCounts
+
+
+
+########################## HELPER FUNCTIONS ###############################################
+
+def preprocess(text):
+    no_punctuation_text = ''
+    if (type(text)== str):
+        lower_text = text.lower()
+        no_punctuation_text = lower_text.translate({ord(c):'' for c in string.punctuation})
+    return no_punctuation_text
 
 def myTokenize(text):
-	'''
-	creates a list of tokens using a SnowballStemmer
-	'''
 	global snowballStemmer
 	snowballStemmer = SnowballStemmer("english", ignore_stopwords=True)
 	tokens = []
@@ -57,142 +159,53 @@ def myTokenize(text):
 	tokens = nltk.word_tokenize(cleaned)
 	filtered = [w for w in tokens if not w in stopwords.words('english')]
 	stemmed = [w for w in map(snowballStemmer.stem, filtered)]
-
 	return stemmed
 
-def preprocess(text):
-	'''
-	turns all text into lowercase and removes punctuation to create tokens
-	'''
-	no_punctuation_text = ''
-	if (type(text)== str):
-		lower_text = text.lower()
-		no_punctuation_text = lower_text.translate({ord(c):'' for c in string.punctuation})
+def truth_value(myRow):
+    if (myRow['ground_truth'] == True and myRow['predicted_value'] == True):
+        return 'truePositive'
+    elif (myRow['ground_truth'] == True and myRow['predicted_value'] == False):
+        return 'falseNegative'
+    elif (myRow['ground_truth'] == False and myRow['predicted_value'] == True):
+        return 'falsePositive'
+    elif (myRow['ground_truth'] == False and myRow['predicted_value'] == False):
+        return 'trueNegative'
+    else:
+        return None
+#Returns top n tfidf features as df, but takes dense format vector as input
+def top_tfidf_feats(row, features, top_n=25):
+    ''' Get top n tfidf values in row and return them with their corresponding feature names.'''
+    topn_ids = np.argsort(row)[::-1][:top_n]
+    top_feats = [(features[i], row[i]) for i in topn_ids]
+    df = pd.DataFrame(top_feats)
+    df.columns = ['feature', 'tfidf']
+    return df
 
-	return no_punctuation_text
+#convert single row into dense format
+def top_feats_in_doc(Xtr, features, row_id, top_n=25):
+    ''' Top tfidf features in specific document (matrix row) '''
+    row = np.squeeze(Xtr[row_id].toarray())
+    return top_tfidf_feats(row, features, top_n)
 
-def Instantiate_TFIDF_Vectorizer():
-	'''
-	creates matrices of TFidf scores for both the training set and the testing set, also returns list of feature names
-	'''
-	##Instantiate a TFidf vectorizer
-	vectorizer = TfidfVectorizer(sublinear_tf=True, encoding='utf-8', 
-                             max_df=0.5, tokenizer=myTokenize)
+def top_mean_feats(Xtr, features, grp_ids=None, min_tfidf=0.1, top_n=25):
+    ''' Return the top n features that on average are most important amongst documents in rows
+        indentified by indices in grp_ids. '''
+    if grp_ids:
+        D = Xtr[grp_ids].toarray()
+    else:
+        D = Xtr.toarray()
 
-	return vectorizer
+    D[D < min_tfidf] = 0
+    tfidf_means = np.mean(D, axis=0)
 
-def Train_Vectorizer(train_df, vectorizer):
-
-	#may have to do feature selection rather than pass entire body
-	train_X = vectorizer.fit_transform(train_df['body'])
-
-	##Save feature names in a separate list
-	feature_names = vectorizer.get_feature_names()
-
-	return train_X, feature_names
-
-def  Test_Vectorizer(test_df, vectorizer):
-	
-	#From http://fastml.com/classifying-text-with-bag-of-words-a-tutorial/
-	#Create another matrix of tfidf scores for the documents in the test set
-	test_X = vectorizer.transform(test_df['body'])
-
-	return test_X
-
-def Logistic_Regression(train_X, test_X, class_labels_training, class_labels_test):
-	'''
-	runs logistic regression
-
-	calculates p value for logistric regression
-
-	prints model output and p value
-	'''
-	model_lr = LR()
-	model_lr.fit(train_X, class_labels_training)
-	print(model_lr)
-
-	p = model_lr.predict_proba( test_X )[:,1]
-	print(model_lr.score( test_X, class_labels_test))
-
-	return p
-
-def Naive_Bayes(train_X, test_X, class_labels_training, class_labels_test):
-	'''
-	runs Naive Bayes model
-
-	calculates p value for Naive Bayes 'p2'
-
-	prints model output and p value
-	'''
-
-	model_nb = MultinomialNB(alpha=0.05)
-	print(model_nb.fit(train_X, class_labels_training))
-
-	p2 = model_nb.predict( test_X )
-	print(model_nb.score( test_X, class_labels_test))
-
-	results = [(class_labels_test[i], p2[i]) for i in range(0,len(p2))]
-	len(results)
-	print(results[0:20])
+    return top_tfidf_feats(tfidf_means, features, top_n)
 
 
-	return results, p2, model_nb
+def top_feats_by_class(Xtr, y, features, min_tfidf=0.1, top_n=25):
+    ''' Return a list of dfs, where each df holds top_n features and their mean tfidf value
+        calculated across documents with the same class label. '''
+    ids = np.where(y.personal==True)
+    feats_df = top_mean_feats(Xtr, features, ids, min_tfidf=min_tfidf, top_n=top_n)
+    feats_df.label = 'personal'
 
-def analyze_results(test_df, p, p2, class_labels_test):
-	'''
-	counts number of true negatives, true positives, false negatives and false positives for logistic
-
-	calculates classifier accuracy and error rates for logistic (calls on functions accuracy and error rate defined below)
-
-	calculates precision and recall for naive bayes
-
-	'''
-	output_df = pd.DataFrame( data = {'messageId': test_df['messageId'], 
-                                  'personal': test_df['personal'], 
-                                  'logistic': p })
-	print("Output dataframe length: {}\n".format(len(output_df)))
-	THRESHOLD = .23
-	output_df['prediction'] = (output_df['logistic'] >= THRESHOLD)
-	print("DF columns:{}\n".format(", ".join(output_df.columns)))
-	print(output_df.head(30))
-
-	true_positives = len(output_df.loc[(output_df['personal'] == True) & (output_df['prediction'] == True)])
-	false_positives = len(output_df.loc[(output_df['personal'] == False) & (output_df['prediction'] == True)])
-	true_negatives = len(output_df.loc[(output_df['personal'] == False) & (output_df['prediction'] == False)])
-	false_negatives = len(output_df.loc[(output_df['personal'] == True) & (output_df['prediction'] == False)])
-	print("Results\nTrue Positives\tTrue_Negatives\tFalse_Positives\False_Negatives\n")
-	print("\t".join(map(str, [true_positives, true_negatives, false_positives, false_negatives])))
-
-	print("Classifier Accuracy: {}\n".format(accuracy(true_positives, true_negatives, 
-                                                  false_positives, false_negatives)))
-	print("Classifier Error Rate: {}\n".format(error_rate(true_positives, true_negatives,
-                                                      false_positives, false_negatives)))
-
-	print(metrics.classification_report(class_labels_test, p2))
-
-	return true_positives, true_negatives, false_negatives, false_positives
-
-
-def accuracy(tp, tn, fp, fn):
-	accuracy_metric = ((tp + tn)/(tp + tn + fp + fn))
-	return accuracy_metric
-
-def error_rate(tp, tn, fp, fn):
-	error_metric = ((fp + fn)/ (tp + tn + fp + fn))
-	return error_metric
-
-def show_most_informative_features(vectorizer, clf, n=20):
-	'''
-	prints list of most informative features
-	'''
-	feature_names = vectorizer.get_feature_names()
-	coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
-	top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
-	for (coef_1, fn_1), (coef_2, fn_2) in top:
-	    print ("\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2))
-
-	return None 
-
-
-
-
+    return feats_df
