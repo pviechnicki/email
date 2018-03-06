86af974d1f4f05d89ca1842b309f7f794d1e0afa
Emily Bicks <embicks@deloitte.com>
2018-03-06 11:40:43 -0500

    pushing requirements.txt

--- viz/mapv3.py
+++ viz/mapv3.py
@@ -11,13 +11,32 @@ import os
 from flask import send_from_directory
 import geopy
 from geopy.geocoders import Nominatim
+import getopt
 
 #create dataframe
+def usage():
+    sys.stdout.write("Usage: python wrangle.py [-d|--directory= <top directory of the github repository where your directory yaml sits>] [-n|--number= <number of output emails requested>] [-h|?|--help]") 
+    
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "d:n:h?", ["--directory=", "--number=", "--help"])
+except getopt.GetoptError as err:
+    #Exit if can't parse args
+    usage() 
+    sys.exit(2)
+for o, a in opts:
+    if (o == '-h' or o == '-?'):
+        usage()
+        exit(0)
+    elif o in ('-d', '--directory'):
+        parent_path = a
+        sys.path.insert(0, parent_path + '//' + 'utils')
+        from load_directories import directory_loader
+        input_directory, output_directory = directory_loader(parent_path)
 
-email_df = pd.read_csv('C:/Users/embicks/Documents/DOTCE/email_marker/email/data/Output/Master_df.csv')
+email_df = pd.read_csv(output_directory + '//' + 'Master_df.csv')
 
 geolocator = Nominatim()
-country_df = email_df.groupby(['country', 'sensitivity'])['messageId'].count()
+country_df = email_df.groupby(['country', 'sensitivity','user_type'])['messageId'].count()
 country_df = country_df.to_frame()
 country_df.reset_index(inplace=True)
 country_df['long'] = ''
@@ -28,6 +47,9 @@ for i, row in country_df.iterrows():
     country_df = country_df.set_value(i, 'long', country.longitude)
     country_df = country_df.set_value(i, 'lat', country.latitude)
 print(country_df.head())
+
+my_sensitivities = list(set(country_df['sensitivity']))
+country_df['display_text'] = country_df['country'] + str(country_df['messageId'])
 
 #Local version of stylesheet from https://codepen.io/chriddyp/pen/bWLwgP.css
 stylesheets = ['bWLwgP.css']
@@ -73,18 +95,131 @@ app.layout = html.Div(children = [
                             lon = country_df['long'],
                             marker = dict(
                             	size = country_df['messageId']),
+                                color = country_df['sensitivity'],
+                            text = country_df['country']
                             	# color = country_df['sensitivity']
                             ) #data dict
                         ], #data
                     "layout": dict(
-                        autosize = True
+                        height = 500,
+                        width = 1000,
+                        geo = dict(
+                            showland = True,
+                            showcoastlines = True,
+                            showocean = True,
+                            showcountries = True,
+                            oceancolor = 'rgb(240,248,255)',
+                            landcolor = 'rgb(128,128,1281)',
+                            countrycolor = "rgb(255, 255, 255)" ,
+                            coastlinecolor = "rgb(255, 255, 255)",
+                            projection = dict(
+                                type = 'Mercator'
+                                )
+                            )
                         ) #layout dict
                     } #figure
             ) #dcc.graph
         ]
+    ),
+        html.Div(
+        id = "bar",
+        children =
+        [
+            dcc.Graph(
+                id='myBar',
+                figure={
+                    "data": [
+                        go.Bar(
+                            x = list((country_df['messageId'])),
+                            y = list(country_df['user_type']),
+                            marker = go.Marker(
+                                color = 'rgb(26,118,255)')
+
+                                # color = country_df['sensitivity']
+                            ) #data dict
+                        ], #data
+                    "layout": dict(
+                        height = 500,
+                        width = 1000,
+                        ) #layout dict
+                    } #figure
+            ) #dcc.graph
+        ]
+    ),
+    html.Div(id='dropdown_div', children= [
+        html.H3("Select Sensitivity of Interest"),
+        dcc.Dropdown
+        (
+            id='my_dropdown',
+            options = [{'label': sensitivity, 'value': sensitivity} for sensitivity in my_sensitivities],
+            value = 'All'
+        )
+    ]
     )
 ])
 
+@app.callback(
+    Output(component_id='myMap', component_property='figure'),
+    [Input(component_id='my_dropdown', component_property='value')]
+)
+
+
+def update_map_data(new_value):
+    filtered_df = country_df[country_df['sensitivity'] == new_value]
+    return {
+        "data": [
+            dict(
+                type = 'scattergeo',
+                mode = 'markers',
+                lat = filtered_df['lat'],
+                lon = filtered_df['long'],
+                marker = dict(
+                    size = filtered_df['messageId']),
+                text = filtered_df['country'],
+                    # color = country_df['sensitivity']
+                ) #data dict
+            ], #data
+        "layout": dict(
+            height = 500,
+            width = 1000,
+            geo = dict(
+                showland = True,
+                showcoastlines = True,
+                showocean = True,
+                showcountries = True,
+                oceancolor = 'rgb(240,248,255)',
+                landcolor = 'rgb(128,128,128)',
+                countrycolor = "rgb(255, 255, 255)" ,
+                coastlinecolor = "rgb(255, 255, 255)",
+                projection = dict(
+                    type = 'Mercator'
+                    )
+                )
+            ) #layout dict
+        } #figure
+@app.callback(
+    Output(component_id='myBar', component_property='figure'),
+    [Input(component_id='my_dropdown', component_property='value')]
+)
+def update_bar_data(new_value):
+    filtered_df = country_df[country_df['sensitivity'] == new_value]
+
+    return {"data": [
+                go.Bar(
+                    x = list(filtered_df['messageId']),
+                    y = list(filtered_df['user_type']),
+                    marker = go.Marker(
+                        color = 'rgb(26,118,255)')
+
+                        # color = country_df['sensitivity']
+                    ) #data dict
+                ], #data
+            "layout": dict(
+                height = 500,
+                width = 1000,
+                )
+        }
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
-
